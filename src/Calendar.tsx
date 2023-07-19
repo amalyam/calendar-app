728b726e746fc7601bc2d1b9b49daaa731dc7e17
Amalya Megerman <amalyamegerman@gmail.com>
2023-07-19 16:07:57 -0400

    refactor date calculations to functions

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,11 +1,25 @@
 import { Container, Paper } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
-export default function Calendar() {
+function calculateFirstDay(): Date {
+  // calculate first day of current month
+
   const today = new Date();
   const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+
+  return firstDay;
+}
+
+function calculateCurrentMonthDay(firstDay: Date): number {
+  // calculate which day of the week the current month starts on
   const firstDayValue = firstDay.getDay();
 
+  return firstDayValue;
+}
+
+function calculateMonthLastDate(firstDay: Date): number {
+  // calculate the last day of the current month
+  // (how many days there are in the current month)
   const nextMonthFirstDay = new Date(
     firstDay.getFullYear(),
     firstDay.getMonth() + 1,
@@ -15,6 +29,13 @@ export default function Calendar() {
   const nextMonthFirstDayUTC = nextMonthFirstDay.valueOf();
   const lastDay = new Date(nextMonthFirstDayUTC - 1);
   const lastDayDate = lastDay.getDate();
+
+  return lastDayDate;
+}
+
+export default function Calendar() {
+  const firstDayValue = calculateCurrentMonthDay(calculateFirstDay());
+  const lastDayDate = calculateMonthLastDate(calculateFirstDay());
 
   return (
     <Container>
