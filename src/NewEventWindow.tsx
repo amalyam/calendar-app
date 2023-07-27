05935cc254b6cec221db314c0b06452636ffdf9a
Amalya Megerman <amalyamegerman@gmail.com>
2023-07-27 13:31:43 -0400

    manage state for event params

--- src/NewEventWindow.tsx
+++ src/NewEventWindow.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import dayjs, { Dayjs } from "dayjs";
 import { Popper, Box, Button, TextField, Typography } from "@mui/material";
 import { DateTimeField } from "@mui/x-date-pickers/DateTimeField";
 
@@ -8,7 +9,11 @@ import { DateTimeField } from "@mui/x-date-pickers/DateTimeField";
 /*repeat options*/
 /*add error prop for event name, etc */
 
-export default function NewEventWindow() {
+export default function NewEventWindow({
+  onSave,
+}: {
+  onSave: (newEvent: CalendarEvent) => void;
+}) {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -17,6 +22,13 @@ export default function NewEventWindow() {
 
   const open = Boolean(anchorEl);
   const id = open ? "simple-popper" : undefined;
+  const now = dayjs();
+
+  const [eventName, setEventName] = useState("");
+  const [startDateTime, setStartDateTime] = useState(dayjs());
+  const [endDateTime, setEndDateTime] = useState(now.hour(now.hour() + 1));
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
 
   return (
     <div className="new-task">
@@ -54,26 +66,69 @@ export default function NewEventWindow() {
       >
         <Box component="form" sx={{ border: 1, p: 1, bgcolor: "white" }}>
           <div>
-            <Typography variant="h3">New Event</Typography>
+            <TextField
+              id="event-name"
+              label="Event Name"
+              value={eventName}
+              onChange={(event) => setEventName(event.target.value)}
+              required
+            />
           </div>
           <div>
-            <TextField id="event-name" label="Event Name"></TextField>
+            <DateTimeField
+              label="start"
+              value={startDateTime}
+              onChange={(event) => setStartDateTime(startDateTime)}
+              format="l LT"
+              required
+            />
           </div>
           <div>
-            <DateTimeField label="start" />
+            <DateTimeField
+              label="end"
+              value={endDateTime}
+              onChange={(event) => setEndDateTime(endDateTime)}
+              format="l LT"
+              required
+            />
           </div>
           <div>
-            <DateTimeField label="end" />
-          </div>
-          <div>
-            <TextField id="description" label="Description">
+            <TextField
+              id="description"
+              label="Description"
+              value={description}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setDescription(event.target.value)
+              }
+            >
               Description
             </TextField>
           </div>
           <div>
-            <TextField id="location" label="Location">
+            <TextField
+              id="location"
+              label="Location"
+              value={location}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setLocation(event.target.value)
+              }
+            >
               Location
             </TextField>
+          </div>
+          <div>
+            <Button variant="outlined" onClick={() => setAnchorEl(null)}>
+              close
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                onSave();
+                setAnchorEl(null);
+              }}
+            >
+              save
+            </Button>
           </div>
         </Box>
       </Popper>
